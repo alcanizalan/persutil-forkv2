0ae326b478c04ca15106d3421839d50ec5911772
Rafael Aznar <rafaaznar@gmail.com>
2025-11-10 12:57:37 +0100

    Add application properties, BlogApi, and database schema for persutil

---
+++ src/main/java/net/ausiasmarch/persutil/api/BlogApi.java
@@ -0,0 +1,19 @@
+package net.ausiasmarch.persutil.api;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogApi {
+
+    @GetMapping("/saludar")
+    public ResponseEntity<String> saludar() {
+        return new ResponseEntity<>("Hola desde el blog", HttpStatus.OK);
+    }
+    
+}
